28c70cc3e05166255828066318be3171cc46bed5
dmschd0678 <63108606+dmschd0678@users.noreply.github.com>
2020-09-01 08:19:55 +0900

    은행 프로그램

--- SCB/src/Learn/Bank.java
+++ SCB/src/Learn/Bank.java
@@ -1,5 +1,67 @@
 package Learn;
 
-public class Bank {
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
+public class Bank {
+	SimpleDateFormat format;
+	Date date;
+	
+	private final int MAX_ACCOUNT = 10;
+	private Person[] accounts;
+	private int size;
+	ArrayList<String> logs = new ArrayList<>();
+	
+	public Bank() {
+		accounts = new Person[MAX_ACCOUNT];
+		logs = new ArrayList<>();
+		size = 0;
+		
+		SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-DD");
+		
+		this.addLog("은행 설립");
+		System.out.println("은행이 설립됨");
+	}
+	
+	public void addLog(String log) {
+		date = new Date();
+		
+		log = format.format(date) + " - " + log;
+		this.logs.add(log);
+	}
+	
+	public String getLog() {
+		String output ="";
+		for(String a : this.logs)
+			output += a +"\n";
+		return output;
+	}
+	public boolean createAccount(Person account) {
+		if(size < 10) {
+			this.accounts[size++] = account;
+			this.addLog("계좌 추가 성공-" + account.toString());
+			
+			return true;
+		}
+		else {
+			if(account instanceof VIP) {
+				for(int i = MAX_ACCOUNT-1; i>=0; i--) {
+					if(!(account instanceof VIP)) {
+						this.addLog("계좌 삭제- " + this.accounts[i].toString());
+						this.accounts[i] = account;
+						this.addLog("계좌 추가 성공 - " + account.toString());
+						
+						return true;
+					}
+				}
+				this.addLog("계좌 추가 실패- " + account.toString());
+				return false;
+			}
+			else {
+				this.addLog("계좌 추가 실패- " + account.toString());
+				return false;
+			}
+		}
+	}
 }
--- SCB/src/Learn/Main.java
+++ SCB/src/Learn/Main.java
@@ -3,11 +3,15 @@ package Learn;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Person person = new Person("123456-1234567","ȫ�浿",1000);
 		VIP vip = new VIP(person,5);
+		
 		person.addMoney(-5000);
+		
 		System.out.println(person.toString());
 		System.out.println(vip.toString());
+
 	}
 
 }
